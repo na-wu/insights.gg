158a91c2f4b702d27d70078f3713fd01e6c06b77
na-wu <nwu1018@gmail.com>
2020-05-21 09:53:37 -0700

    more prettier

--- src/components/woofDialog.js
+++ src/components/woofDialog.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
+import React from "react";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
 
 export default function AlertDialog(props) {
-  console.log(props.bool)
+  console.log(props.bool);
   const handleClose = () => {
-    props.cb()
+    props.cb();
   };
 
   return (
