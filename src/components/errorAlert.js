1c39c2072f49d3cebbc8bbcefbdd7f0e829a2567
na-wu <nwu1018@gmail.com>
2020-05-21 09:52:08 -0700

    more prettier

--- src/components/errorAlert.js
+++ src/components/errorAlert.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Alert from '@material-ui/lab/Alert';
-import IconButton from '@material-ui/core/IconButton';
-import Collapse from '@material-ui/core/Collapse';
-import Button from '@material-ui/core/Button';
-import CloseIcon from '@material-ui/icons/Close';
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Alert from "@material-ui/lab/Alert";
+import IconButton from "@material-ui/core/IconButton";
+import Collapse from "@material-ui/core/Collapse";
+import Button from "@material-ui/core/Button";
+import CloseIcon from "@material-ui/icons/Close";
 
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: '100%',
-    '& > * + *': {
+    width: "100%",
+    "& > * + *": {
       marginTop: theme.spacing(2),
     },
   },
@@ -17,15 +17,15 @@ const useStyles = makeStyles((theme) => ({
 
 export default function DescriptionAlerts(props) {
   const classes = useStyles();
-  console.log(props.bool)
+  console.log(props.bool);
 
   const [open, setOpen] = React.useState(props.bool);
-  
+
   return (
     <div className={classes.root}>
       <Collapse in={open}>
         <Alert
-        severity="error"
+          severity="error"
           action={
             <IconButton
               aria-label="close"
